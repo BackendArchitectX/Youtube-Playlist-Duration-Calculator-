6d2ef68541027234c7c4648790b69ef519ebd10c
Pranay Kadu <pranayp.kadu@gmail.com>
2026-01-02 16:13:06 +0530

    Updated

--- demo/src/main/java/com/youtubeplaylistduration/YoutubePlaylistDurationApplication.java
+++ demo/src/main/java/com/youtubeplaylistduration/YoutubePlaylistDurationApplication.java
@@ -11,5 +11,6 @@ public class YoutubePlaylistDurationApplication {
 	}
 }
 /*
-A YouTube playlist duration calculator using Java Spring Boot is a web application that allows users to input a YouTube playlist URL and calculates the total duration of all videos in that playlist. 
+A YouTube playlist duration calculator using Java Spring Boot is a web application
+that allows users to input a YouTube playlist URL and calculates the total duration of all videos in that playlist.
 */
